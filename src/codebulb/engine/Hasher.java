196fde09a46ce0fc9fd1a19ac867487d34262c3a
Robert Lemmens <robert_lemmens@hotmail.com>
2014-10-06 22:23:34 +0200

    MD5 paneel aangepast. Textfields en knop toegevoegd. Methodes in Hasher toegevoegd

--- src/codebulb/engine/Hasher.java
+++ src/codebulb/engine/Hasher.java
@@ -1,8 +1,32 @@
 package codebulb.engine;
 
+import java.io.File;
+
 /**
  * Created by Robert Lemmens on 6-10-2014.
  */
 public class Hasher {
+
+    private File file;
+
+    public Hasher(File file) {
+        this.file = file;
+    }
+
+    public void hashMD5() {
+
+    }
+
+    public void hashSHA1() {
+
+    }
+
+    public void hashSHA256() {
+
+    }
+
+    public void hashSHA512() {
+
+    }
 
 }
--- src/codebulb/frames/MD5Paneel.java
+++ src/codebulb/frames/MD5Paneel.java
@@ -1,6 +1,7 @@
 package codebulb.frames;
 
 import javax.swing.*;
+import java.awt.*;
 
 /**
  * Created by Robert Lemmens on 6-10-2014.
@@ -8,14 +9,44 @@ import javax.swing.*;
 
 public class MD5Paneel extends JPanel {
 
-    private JButton button1 = new JButton("Test");
+    private JButton browse;
+
+    private JTextArea md5;
+    private JLabel md5Label;
+    private JTextArea file;
+    private JLabel fileLabel;
 
     public MD5Paneel() {
         setLayout(null);
+        setBackground(Color.DARK_GRAY);
 
-        button1.setSize(100,20);
-        button1.setLocation(200,200);
+        md5 = new JTextArea();
+        md5.setSize(200,20);
+        md5.setLocation(10,300);
 
-        add(button1);
+        md5Label = new JLabel("Paste md5 string:");
+        md5Label.setSize(200,20);
+        md5Label.setLocation(10, 280);
+        md5Label.setForeground(Color.white);
+
+        file = new JTextArea();
+        file.setSize(200,20);
+        file.setLocation(275,300);
+
+        fileLabel = new JLabel("Browse file:");
+        fileLabel.setSize(200,20);
+        fileLabel.setLocation(275,280);
+        fileLabel.setForeground(Color.white);
+        
+
+        browse = new JButton("Browse");
+        browse.setSize(100,20);
+        browse.setLocation(375,322);
+
+        add(md5);
+        add(md5Label);
+        add(file);
+        add(fileLabel);
+        add(browse);
     }
 }
