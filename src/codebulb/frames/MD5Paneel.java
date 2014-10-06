a02360e8ef47d538df96aa6debd86a685450c904
Robert Lemmens <robert_lemmens@hotmail.com>
2014-10-07 00:23:47 +0200

    Opmaak veranderd, drag & drop toegevoegd, in test fase

--- src/codebulb/frames/MD5Paneel.java
+++ src/codebulb/frames/MD5Paneel.java
@@ -1,6 +1,7 @@
 package codebulb.frames;
 
 import codebulb.engine.Hasher;
+import codebulb.utility.FileDrop;
 import codebulb.utility.ReadFiles;
 
 import javax.swing.*;
@@ -21,7 +22,6 @@ public class MD5Paneel extends JPanel implements ActionListener{
     private JLabel md5Label;
     private JTextArea file;
     private JLabel fileLabel;
-
     private JLabel fileMD5;
 
     public MD5Paneel() {
@@ -41,14 +41,15 @@ public class MD5Paneel extends JPanel implements ActionListener{
         file.setSize(200,20);
         file.setLocation(275,300);
 
+
         fileLabel = new JLabel("Browse file:");
         fileLabel.setSize(200,20);
         fileLabel.setLocation(275,280);
         fileLabel.setForeground(Color.white);
 
-        fileMD5 = new JLabel("test");
-        fileMD5.setSize(200,20);
-        fileMD5.setLocation(275,350);
+        fileMD5 = new JLabel("");
+        fileMD5.setSize(300,20);
+        fileMD5.setLocation(120,350);
         fileMD5.setForeground(Color.white);
 
 
@@ -63,21 +64,40 @@ public class MD5Paneel extends JPanel implements ActionListener{
         add(fileLabel);
         add(browse);
         add(fileMD5);
+
+
+        JPanel myPanel = new JPanel();
+        myPanel.setSize(200,200);
+        myPanel.setLocation(10,10);
+        myPanel.setBackground(Color.red);
+        new FileDrop( myPanel, new FileDrop.Listener() {
+             public void filesDropped( java.io.File[] files ) {
+                for(int i = 0; i < files.length; i++) {
+                    System.out.println(files[i].getAbsoluteFile());
+                }
+             }
+             // end filesDropped
+        });
+        // end FileDrop.Listener
+        add(myPanel);
     }
 
     @Override
     public void actionPerformed(ActionEvent e) {
         if(e.getSource().equals(browse)){
+            // create and open a filechooser
             ReadFiles reader = new ReadFiles();
             File file = reader.chooseFile();
+
+            // create a hashing object from the file
             Hasher hasher = new Hasher(file);
             this.file.setText(file.getAbsolutePath());
 
-            // create and get md5
+            // create and get md5 from hasher
             String md5 = hasher.getMD5Checksum();
 
-            // set text to display the user
-            fileMD5.setText(md5);
+            // display md5 to user
+            fileMD5.setText("md5: " + md5);
         }
     }
 }
