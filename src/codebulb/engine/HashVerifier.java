216f2c9b22d87231465058b4448ee3554d59322e
Robert Lemmens <robert_lemmens@hotmail.com>
2014-10-07 15:21:26 +0200

    Code opgeruimt. Multi-drop support ge-add. TODO - Manier voor multidrop MD5 checking toevoegen

--- src/codebulb/engine/HashVerifier.java
+++ src/codebulb/engine/HashVerifier.java
@@ -1,9 +1,29 @@
 package codebulb.engine;
 
+import javax.swing.*;
+import java.awt.*;
+
 /**
  * Created by Robert Lemmens on 6-10-2014.
  */
-public class HashVerifier {
+public class HashVerifier extends Component {
+
+    public HashVerifier() {
+
+    }
+
+    public boolean md5Check(String twee, String een) {
+        System.out.println(een);
+        System.out.println(twee);
+        if(twee.equals(een)){
+            System.out.println("Matched");
+            return true;
+        } else{
+            System.out.println("Geen match");
+            return false;
+        }
+
+    }
 
     // stuff  stuff stuff merge test
 }
