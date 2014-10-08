12d7c8e1af19f2ccdb2b4c1facd0c1c301630489
Robert Lemmens <robert_lemmens@hotmail.com>
2014-10-08 14:22:43 +0200

    Classes aangepast. Seperated creating sums and checking sums in a better way. General code changes (refactors e.d.)

--- src/codebulb/frames/MD5Paneel.java
+++ src/codebulb/frames/MD5Paneel.java
@@ -15,11 +15,11 @@ import java.util.ArrayList;
  * Created by Robert Lemmens on 6-10-2014.
  */
 
-public class MD5Paneel extends JPanel implements ActionListener, MouseListener{
+public class MD5Paneel extends JPanel implements ActionListener, MouseListener, KeyListener{
 
-    private JButton browse;
+    private JButton check;
     private JPanel dropPanel;
-    private JTextArea md5Field;
+    private JTextArea md5Field[];
     private JLabel md5Label;
 
     private JTextArea file;
@@ -31,33 +31,48 @@ public class MD5Paneel extends JPanel implements ActionListener, MouseListener{
     private ArrayList<String> md5Lijst;
     private ArrayList<String> fileLijst;
 
+    private int fieldCounter = 1;
+
     public MD5Paneel() {
         setLayout(null);
         setBackground(Color.DARK_GRAY);
 
         md5Lijst = new ArrayList<String>();
         fileLijst = new ArrayList<String>();
+        Dimension fieldDim = new Dimension(140,240);
+        // single file support only -
+        // experimental - have to convert to arraylist sooner or later.
+        md5Field = new JTextArea[100];
+        for(int i = 0; i < 5; i++) {
+            md5Field[i] = new JTextArea();
+            md5Field[i].setSize(220, 20);
+            md5Field[i].setLocation(fieldDim.width, fieldDim.height + (40*i));
+            md5Field[i].setText("");
+            md5Field[i].addKeyListener(this);
+            add(md5Field[i]);
+            System.out.println("added " + i);
+        }
 
-        // single file support only - 
-        md5Field = new JTextArea();
-        md5Field.setSize(220, 20);
-        md5Field.setLocation(140, 300);
-        md5Field.setText("");
+        check = new JButton("Check");
+        check.setSize(100,20);
+        check.setLocation(380, 400);
+        check.addActionListener(this);
+        add(check);
 
         md5Label = new JLabel("Paste md5 string:");
         md5Label.setSize(200,20);
-        md5Label.setLocation(150, 280);
+        md5Label.setLocation(150, 220);
         md5Label.setForeground(Color.white);
 
-        fileMD5 = new JLabel("");
-        fileMD5.setSize(300,20);
-        fileMD5.setLocation(120,350);
-        fileMD5.setForeground(Color.white);
+        // label showing MD5 of first file added.
+        /*
+        REMOVED
+         */
 
 
-        add(md5Field);
+
         add(md5Label);
-        add(fileMD5);
+
 
         // image path moet nog project relatief gemaakt worden
         ImageIcon uploadIcon = new ImageIcon("C:\\Users\\Robert\\IdeaProjects\\JVerify-1.0\\res\\images\\small65.png");
@@ -79,9 +94,9 @@ public class MD5Paneel extends JPanel implements ActionListener, MouseListener{
                  // grab files ( Multiple files may be added but hashing is not yet supported.
                 for(int i = 0; i < files.length; i++) {
                     // create and compare (if possible) md5 sums
-                    boolean isMatch = MD5Event(files[i]);
+                    MD5Event(files[i]);
                     // print the result
-                    System.out.println(isMatch);
+
                 }
                  /*
                  OPEN EEN FRAME MET RESULTS NADAT DE LISTENER KLAAR IS MET FILES ADDEN / BEWERKEN
@@ -104,28 +119,30 @@ public class MD5Paneel extends JPanel implements ActionListener, MouseListener{
          Browse knop bestaat niet meer! Alle interactie is over naar het drop paneel = dropPanel, zie mouselistener
          */
         System.out.println("ACTION");
+        for(int i = 0; i < md5Lijst.size(); i++) {
+            boolean temp = matcher(md5Lijst.get(i), md5Field[i].getText());
+            System.out.println("file " + i +": " + temp);
+        }
     }
 
-    public boolean MD5Event(File file) {
+    public void MD5Event(File file) {
         // create a hashing object from the file
         Hasher hasher = new Hasher(file);
 
         // create and get md5 from hasher
         String md5 = hasher.getMD5Checksum();
-        String inputmd5 = md5Field.getText();
-
-        // display md5 to user
-        fileMD5.setText("md5: " + md5);
-
-        // create verify object and compare md5. Returns true if matched
-        HashVerifier verify = new HashVerifier();
-        boolean isMatch = verify.md5Check(md5, inputmd5);
 
         // return the result en sla de generated MD5 van de file op in de lijst.
         fileLijst.add(file.getAbsolutePath());
         md5Lijst.add(md5);
 
+    }
 
+    public boolean matcher(String md5, String fieldmd5) {
+        // create verify object and compare md5. Returns true if matched
+        HashVerifier verify = new HashVerifier();
+        String inputmd5 = md5Field[0].getText();
+        boolean isMatch = verify.md5Check(md5, fieldmd5);
 
         return isMatch;
     }
@@ -137,9 +154,7 @@ public class MD5Paneel extends JPanel implements ActionListener, MouseListener{
         File file = reader.chooseFile();
 
         // create and compare (if textfield is not empty) md5 sums
-        boolean isMatch = MD5Event(file);
-
-        System.out.println(isMatch);
+        MD5Event(file);
 
 
     }
@@ -161,6 +176,26 @@ public class MD5Paneel extends JPanel implements ActionListener, MouseListener{
 
     @Override
     public void mouseExited(MouseEvent e) {
+
+    }
+
+    @Override
+    public void keyTyped(KeyEvent e) {
+
+    }
+
+    @Override
+    public void keyPressed(KeyEvent e) {
+        int key = e.getKeyCode();
+        switch(key) {
+            case KeyEvent.VK_ENTER:
+
+                break;
+        }
+    }
+
+    @Override
+    public void keyReleased(KeyEvent e) {
 
     }
 }
